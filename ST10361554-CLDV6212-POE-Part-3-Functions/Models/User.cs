7210a016df92bf8045d14d5e4716c10dce8930a3
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-04 22:39:38 +0200

    Expand User class with additional properties
    The `User` class in the `ST10361554_CLDV6212_POE_Part_3_Functions.Models` namespace has been significantly expanded. The following properties have been added to the `User` class:
    - `Id`
    - `Role`
    - `Username`
    - `PasswordHash`
    - `Email`
    - `Name`
    - `StreetAddress`
    - `City`
    - `Province`
    - `PostalCode`
    - `Country`
    - `PhoneNumber`
    
    This change enhances the class by providing a more comprehensive representation of user information.

--- ST10361554-CLDV6212-POE-Part-3-Functions/Models/User.cs
+++ ST10361554-CLDV6212-POE-Part-3-Functions/Models/User.cs
@@ -8,5 +8,28 @@ namespace ST10361554_CLDV6212_POE_Part_3_Functions.Models
 {
     public class User
     {
+        public string Id { get; set; }
+
+        public string Role { get; set; }
+
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
