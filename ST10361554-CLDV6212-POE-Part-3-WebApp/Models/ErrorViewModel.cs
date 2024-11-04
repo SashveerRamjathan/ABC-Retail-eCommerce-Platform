49146aa6a246f3458a3e633530b1ac1d6906255e
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-04 21:42:57 +0200

    Add project files.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
