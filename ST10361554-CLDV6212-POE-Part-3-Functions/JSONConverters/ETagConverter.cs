0fee2b0c9222dc4955d845ece63ddf36e5af0d6c
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-04 21:59:09 +0200

    Update project files and add ETagConverter class
    Updated project files to include new package references for Azure Storage and Newtonsoft.Json. Updated versions of existing Azure Functions packages. Added ETagConverter class for custom JSON serialization of ETag type.

---
+++ ST10361554-CLDV6212-POE-Part-3-Functions/JSONConverters/ETagConverter.cs
@@ -0,0 +1,29 @@
+﻿using Azure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_CLDV6212_POE_Part_3_Functions.JSONConverters
+{
+    public class ETagConverter : JsonConverter<ETag>
+    {
+        public override ETag ReadJson(JsonReader reader, Type objectType, ETag existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            // Read the value from the JSON and convert it to a string
+            var eTagString = reader.Value?.ToString();
+
+            // Create a new ETag instance from the string
+            return string.IsNullOrEmpty(eTagString) ? default : new ETag(eTagString);
+        }
+
+        // WriteJson method to serialize the ETag to JSON
+        public override void WriteJson(JsonWriter writer, ETag value, JsonSerializer serializer)
+        {
+            // Write the ETag value as a string to JSON
+            writer.WriteValue(value.ToString());
+        }
+    }
+}
