41bc072d587c8147d5ab85be9a905d35d05c0a17
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 00:45:17 +0200

    Add OrderId property to Order entity and update migration
    Updated ApplicationDbContextModelSnapshot.cs to include a new
    required property OrderId in the Order entity with column type
    nvarchar(max). Added a new migration file 20241105224313_UpdateOrderTable.cs
    to add the OrderId column to the Orders table in the database,
    ensuring it is not nullable with a default value of an empty string.
    Generated 20241105224313_UpdateOrderTable.Designer.cs to reflect
    these changes in the model snapshot.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Migrations/20241105224313_UpdateOrderTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Orders");
+        }
+    }
+}
