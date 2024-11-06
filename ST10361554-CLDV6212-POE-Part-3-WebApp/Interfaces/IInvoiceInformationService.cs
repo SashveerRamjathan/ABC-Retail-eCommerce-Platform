1d6a5d9af9755b562ab4454cb964bdb8203c1eef
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:13:15 +0200

    Add IInvoiceInformationService interface with async method
    Added a using directive for the ST10361554_CLDV6212_POE_Part_3_WebApp.Models namespace.
    Introduced a new namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces.
    Created IInvoiceInformationService interface.
    Defined GetInvoiceInfoAsync method to fetch invoice details asynchronously.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/IInvoiceInformationService.cs
@@ -0,0 +1,11 @@
+﻿using ST10361554_CLDV6212_POE_Part_3_WebApp.Models;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    // Interface for retrieving invoice information
+    public interface IInvoiceInformationService
+    {
+        // Get the invoice information for a specific user and order
+        Task<InvoiceModel?> GetInvoiceInfoAsync(string userId, string orderId);
+    }
+}
