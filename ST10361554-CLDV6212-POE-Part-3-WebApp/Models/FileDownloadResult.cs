a4c4a8b09c2300482570d9d14be67932e2807ab3
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:03:18 +0200

    Add FileDownloadResult class to Models namespace
    A new class `FileDownloadResult` has been added to the `ST10361554_CLDV6212_POE_Part_3_WebApp.Models` namespace. This class represents the result of a file download operation and includes three required properties: `Content` (a `MemoryStream` containing the file's content), `ContentType` (a `string` specifying the MIME type of the file), and `FileName` (a `string` specifying the name of the file to be downloaded).

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Models/FileDownloadResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Models
+{
+    // Class representing the result of a file download
+    public class FileDownloadResult
+    {
+        public required MemoryStream Content { get; set; } // The content of the file as a memory stream
+        public required string ContentType { get; set; } // The MIME type of the file
+        public required string FileName { get; set; } // The name of the file to be downloaded
+    }
+}
