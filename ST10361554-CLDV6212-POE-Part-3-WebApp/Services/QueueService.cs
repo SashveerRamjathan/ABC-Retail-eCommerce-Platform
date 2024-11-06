eb57ab72edc5966d182884d11ec2beca503e9514
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:21:56 +0200

    Add QueueService class implementing IQueueService interface
    A new QueueService class has been added to the
    ST10361554_CLDV6212_POE_Part_3_WebApp.Services namespace.
    This class implements the IQueueService interface.
    Additionally, the using directive for
    ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces has been added.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Services/QueueService.cs
@@ -0,0 +1,8 @@
+﻿using ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Services
+{
+    public class QueueService : IQueueService
+    {
+    }
+}
