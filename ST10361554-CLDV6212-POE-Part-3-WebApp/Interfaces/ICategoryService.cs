ebb3475ece476cf9f58e9f4d7a17d4b4fac85b45
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:00:42 +0200

    Add ICategoryService interface with GetCategories method
    Introduced a new interface ICategoryService in the namespace
    ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces. This interface
    includes the GetCategories method, which returns a list of
    SelectListItem objects for use in dropdown menus. Added a using
    directive for Microsoft.AspNetCore.Mvc.Rendering to access
    SelectListItem.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    // Interface for category-related operations
+    public interface ICategoryService
+    {
+        // Retrieve a list of categories as SelectListItem objects for use in dropdowns
+        List<SelectListItem> GetCategories();
+    }
+}
