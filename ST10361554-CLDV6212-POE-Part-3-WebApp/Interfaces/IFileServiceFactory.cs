e8c53b749bbd78608046abc16d1434dc39cb494b
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:06:56 +0200

    Add IFileServiceFactory interface for FileService creation
    Introduced a new IFileServiceFactory interface in the
    ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces namespace.
    This interface includes a GetFileService method that takes a
    customerId parameter and returns a FileService instance.
    Imported ST10361554_CLDV6212_POE_Part_3_WebApp.Services
    namespace to utilize the FileService class.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/IFileServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿using ST10361554_CLDV6212_POE_Part_3_WebApp.Services;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    public interface IFileServiceFactory
+    {
+        // Factory interface for creating instances of FileService
+        public interface IFileServiceFactory
+        {
+            // Get an instance of FileService for a specific customer by their ID
+            FileService GetFileService(string customerId);
+        }
+    }
+}
