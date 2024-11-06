7e48b8c2228d7acbdd94c93d59e4bb99317809ce
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:07:49 +0200

    Add IInvoiceGenerationService interface
    Introduced a new namespace `ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces`.
    Added `IInvoiceGenerationService` interface with `GenerateInvoiceAsync` method.
    This method generates an invoice for a given user and order, returning it as a byte array.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/IInvoiceGenerationService.cs
@@ -0,0 +1,9 @@
+﻿namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    // Interface for invoice generation operations
+    public interface IInvoiceGenerationService
+    {
+        // Generate an invoice for a specific user and order, returning the invoice as a byte array
+        Task<byte[]> GenerateInvoiceAsync(string userId, string orderId);
+    }
+}
