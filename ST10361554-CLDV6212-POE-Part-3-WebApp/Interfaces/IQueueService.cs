27b38baedb8a0941f83762cbd23620d02e6db49e
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:21:40 +0200

    Add IQueueService interface for queue operations
    Introduced a new namespace `ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces` and created the `IQueueService` interface within it. The interface includes a `SendMessageAsync` method for sending messages to a specified queue and returning a `SendReceipt`. Added a using directive for `Azure.Storage.Queues.Models` to utilize the `SendReceipt` class.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/IQueueService.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Queues.Models;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    // Interface for queue-related operations
+    public interface IQueueService
+    {
+        // Send a message to a specified queue and return the send receipt
+        Task<SendReceipt> SendMessageAsync(string message, string queueName);
+    }
+}
