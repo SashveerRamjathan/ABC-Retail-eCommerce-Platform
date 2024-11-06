bb2f919805c9b18ff50e63a9651b79a36025a44f
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-06 23:22:10 +0200

    Add IQueueServiceFactory interface for QueueService creation
    Introduced a new namespace `ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces` and defined the `IQueueServiceFactory` interface within it. The interface includes a `Create` method for generating `QueueService` instances based on the provided queue name. Added a using directive for the `ST10361554_CLDV6212_POE_Part_3_WebApp.Services` namespace to support these changes.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Interfaces/IQueueServiceFactory.cs
@@ -0,0 +1,11 @@
+﻿using ST10361554_CLDV6212_POE_Part_3_WebApp.Services;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces
+{
+    // Factory interface for creating instances of QueueService
+    public interface IQueueServiceFactory
+    {
+        // Create an instance of QueueService for a specified queue name
+        QueueService Create(string queueName);
+    }
+}
