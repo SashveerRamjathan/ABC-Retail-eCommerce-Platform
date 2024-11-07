e0687ceaddb98a6528319169c3f23e5a401bcaed
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-07 23:10:43 +0200

    Add OrderDate column to Orders table via migration
    Introduce migration `20241107210909_UpdateOrderTableAddDate` to add `OrderDate` column of type `datetime2` to `Orders` table.
    Update `20241107210909_UpdateOrderTableAddDate.Designer.cs` to include `OrderDate` in the Entity Framework Core model.
    Implement `Up` and `Down` methods in `20241107210909_UpdateOrderTableAddDate.cs` for adding and removing `OrderDate` column.
    Update `ApplicationDbContextModelSnapshot.cs` to reflect the new `OrderDate` column in the `Orders` table.

--- ST10361554-CLDV6212-POE-Part-3-WebApp/Migrations/20241107210909_UpdateOrderTableAddDate.Designer.cs
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Migrations/20241107210909_UpdateOrderTableAddDate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ST10361554_CLDV6212_POE_Part_3_WebApp.Data;
 
@@ -11,9 +12,11 @@ using ST10361554_CLDV6212_POE_Part_3_WebApp.Data;
 namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241107210909_UpdateOrderTableAddDate")]
+    partial class UpdateOrderTableAddDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
