be76ac70974ddcb084827ab947ac6a5b946fe1c9
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-07 21:24:38 +0200

    Add FileServiceFactory class and implement IFileServiceFactory
    Added a new FileServiceFactory class in the ST10361554_CLDV6212_POE_Part_3_WebApp.Services namespace. Implemented the IFileServiceFactory interface in the FileServiceFactory class. Introduced private readonly fields for ILogger<FileService>, IQueueServiceFactory, IHttpClientFactory, and IConfiguration. Added a constructor to initialize these fields. Implemented the GetFileService method to create and configure a new instance of FileService with the provided customerId, setting the customer directory for the FileService instance before returning it.

---
+++ ST10361554-CLDV6212-POE-Part-3-WebApp/Services/FileServiceFactory.cs
@@ -0,0 +1,39 @@
+﻿using ST10361554_CLDV6212_POE_Part_3_WebApp.Interfaces;
+
+namespace ST10361554_CLDV6212_POE_Part_3_WebApp.Services
+{
+    // Factory class for creating instances of FileService
+    public class FileServiceFactory : IFileServiceFactory
+    {
+        private readonly ILogger<FileService> _logger;
+        private readonly IQueueServiceFactory _queueServiceFactory;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        // Constructor to initialize the factory with connection string, logger, and queue service factory
+        public FileServiceFactory(
+            ILogger<FileService> logger,
+            IQueueServiceFactory queueServiceFactory,
+            IHttpClientFactory clientFactory,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _queueServiceFactory = queueServiceFactory;
+            _httpClientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
+        // Method to create and configure a new instance of FileService
+        public FileService GetFileService(string customerId)
+        {
+            // Create a new instance of the FileService class
+            var fileService = new FileService(_logger, _queueServiceFactory, _httpClientFactory, _configuration);
+
+            // Set the customer directory
+            fileService.SetCustomerDirectory(customerId);
+
+            // Return the configured file service
+            return fileService;
+        }
+    }
+}
