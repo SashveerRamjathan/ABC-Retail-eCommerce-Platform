5b1c1072d5193b19ef50e167556594c0fdd25d4e
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-09 15:06:09 +0200

    Update comments in Order class for clarity and consistency
    Revised comments for properties in the Order class to improve
    clarity and ensure consistency. Changes include updating the
    terminology for primary key, order id, product id, quantity,
    total item price, item status, and order date.

--- ST10361554-CLDV6212-POE-Part-3-Functions/Models/Order.cs
+++ ST10361554-CLDV6212-POE-Part-3-Functions/Models/Order.cs
@@ -8,20 +8,20 @@ namespace ST10361554_CLDV6212_POE_Part_3_Functions.Models
 {
     public class Order
     {
-        public string Id { get; set; } // row key
+        public string Id { get; set; } // Primary key
 
-        public string OrderId { get; set; } // order id
+        public string OrderId { get; set; } // Order id
 
-        public string ProductId { get; set; } // product id
+        public string ProductId { get; set; } // Product id
 
         public string UserId { get; set; } // id of the user who placed the order
 
-        public int Quantity { get; set; }
+        public int Quantity { get; set; } // quantity of item ordered
 
-        public decimal TotalItemPrice { get; set; } // total price of the ordered quantity of items
+        public decimal TotalItemPrice { get; set; } // total price of the ordered quantity of item
 
-        public string ItemStatus { get; set; } // status of the order
+        public string ItemStatus { get; set; } // status of the order item
 
-        public DateTime OrderDate { get; set; }
+        public DateTime OrderDate { get; set; } // date the order was placed
     }
 }
